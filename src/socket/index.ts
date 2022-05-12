f6333301d3758d04197af95649355d8416869eee
coreight98 <maxcha98@naver.com>
2022-05-12 14:42:16 +0900

    feat : character & message 캐릭터 이동, 춤 모션 로직 , 상황별 메시지 전송 로직 구현

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -6,212 +6,197 @@ import defaultMapSetting from "../utils/defaultMapSetting";
 const nearByDistance = 100;
 const GATHER_ROOM_CODE: string = "ajou-gather";
 interface IUser {
-	name: string;
-	id: string;
-	cId: string;
+  name: string;
+  id: string;
+  cId: number;
 }
 
 interface IUserList {
-	id: string;
-	socket: string;
+  id: string;
+  socket: string;
 }
 interface IChat {
-	text: string;
-	name: string;
-	cId: string;
-	date: number;
-	id: string;
+  status: string;
+  text: string;
+  name: string;
+  cId: number;
+  date: number;
+  id: string;
 }
 interface IUserData {
-	userId: string;
-	roomCode: string;
-	x: number;
-	y: number;
-	direction: string;
-	state: string;
+  name: string;
+  cId: number;
+  x: number;
+  y: number;
+  direction: string;
+  state: string;
 }
 
 let RoomList: any = {};
 let RoomUser: any = {};
 let userSocketList: IUserList[] = [];
 
-RoomList[GATHER_ROOM_CODE] = [];
+RoomList[GATHER_ROOM_CODE] = 0;
 RoomUser[GATHER_ROOM_CODE] = {};
+
 const createRoomCode = () => {
-	while (true) {
-		const code = Math.random().toString(16).substr(2, 5);
-		if (!(code in Object.keys(RoomList))) return code;
-	}
+  while (true) {
+    const code = Math.random().toString(16).substr(2, 5);
+    if (!(code in Object.keys(RoomList))) return code;
+  }
 };
 
-const findDefaultCharacterPosition = (mapId: number) => defaultMapSetting[mapId].character;
+const findDefaultCharacterPosition = (mapId: number) =>
+  defaultMapSetting[mapId].character;
 
-// const findSocket = (id: number) =>
-// 	userSocketList.reduce((result: string, element: IUserList) => {
-// 		if (element.id === id) result = element.socket;
-// 		return result;
-// 	}, "");
+const findSocket = (id: string) =>
+  userSocketList.reduce(
+    (result: string, element: IUserList) =>
+      element.id === id ? element.socket : result,
+    ""
+  );
 
-const includeUser = (list: any, user: any) => {
-	if (
-		user.x - nearByDistance <= list.x &&
-		list.x <= user.x + nearByDistance &&
-		user.y - nearByDistance <= list.y &&
-		list.y <= user.y + nearByDistance
-	)
-		return true;
-	else return false;
-};
+const includeUser = (list: any, user: any) =>
+  user.x - nearByDistance <= list.x &&
+  list.x <= user.x + nearByDistance &&
+  user.y - nearByDistance <= list.y &&
+  list.y <= user.y + nearByDistance;
 
 const isContain = (tempBackground: any, mapId: number) => {
-	const temp = defaultMapSetting[mapId].defaultMargin;
-	if (
-		temp.minX <= tempBackground.top &&
-		tempBackground.top <= temp.maxX &&
-		temp.minY <= tempBackground.left &&
-		tempBackground.left <= temp.maxY
-	) {
-		return true;
-	} else return false;
+  const temp = defaultMapSetting[mapId].defaultMargin;
+  return (
+    temp.minX <= tempBackground.top &&
+    tempBackground.top <= temp.maxX &&
+    temp.minY <= tempBackground.left &&
+    tempBackground.left <= temp.maxY
+  );
 };
 
-// const findAdjacentUserList = (user: any, code: string) =>
-// 	Object.keys(RoomUser[code]).reduce((result: string[], element: any) => {
-// 		if (element !== user.id) {
-// 			if (includeUser(RoomUser[code][element], RoomUser[code][user.id])) {
-// 				result.push(findSocket(element));
-// 			}
-// 		} else result.push(findSocket(element));
-// 		return result;
-// 	}, []);
+const findAdjacentUserList = (user: IUser, code: string) =>
+  Object.entries(RoomUser[code]).reduce((result: string[], [keys, room]) => {
+    if (keys !== user.id) {
+      if (includeUser(room, user)) {
+        result.push(findSocket(keys));
+      }
+    } else result.push(findSocket(keys));
+    return result;
+  }, []);
 
-const getSocket = (socketIO: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) => {
-	socketIO.on("connect", (socket: Socket) => {
-		console.log("socket connect", socket.id);
+const getSocket = (
+  socketIO: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
+) => {
+  socketIO.on("connect", (socket: Socket) => {
+    console.log("socket connect", socket.id);
 
-		// socket.on(TYPE.CREATE_ROOM, (done: (arg0: { status: string; message: string; code: string }) => void) => {
-		// 	//방 Object 생성
-		// 	const code = createRoomCode();
-		// 	RoomList[code] = [];
-		// 	RoomUser[code] = {};
-		// 	done({
-		// 		status: "SUCESS",
-		// 		message: "성공적으로 방을 만들었습니다.",
-		// 		code,
-		// 	});
-		// });
+    socket.on(
+      TYPE.CREATE_ROOM,
+      (
+        done: (arg0: { status: string; message: string; code: string }) => void
+      ) => {
+        const code = GATHER_ROOM_CODE;
+        if (RoomList[code] !== undefined)
+          done({ status: "FAILED", message: "방이 존재합니다.", code });
+        else {
+          RoomList[code] = 0;
+          RoomUser[code] = {};
+          done({
+            status: "SUCESS",
+            message: "성공적으로 방을 만들었습니다.",
+            code,
+          });
+        }
+      }
+    );
 
-		// socket.on(TYPE.JOIN_ROOM, (user: IUser, code: string) => {
-		// 	socket.join(code);
-		// 	userSocketList.push({ id: user.id, socket: socket.id });
-		// 	if (user.id) {
-		// 		RoomUser[code][user.id] = {
-		// 			name: user.name,
-		// 			x: 1800,
-		// 			y: 1800,
-		// 			direction: "down",
-		// 			state: "mid",
-		// 			cId: user.cId,
-		// 		};
-		// 		socketIO.to(code).emit("makeRoomClient", code, RoomUser[code]);
-		// 	}
-		// });
+    socket.on(
+      TYPE.JOIN_ROOM,
+      (user: IUser, code: string, done: (userData: IUserData) => void) => {
+        socket.join(code);
+        userSocketList.push({ id: user.id, socket: socket.id });
+        if (user.id) {
+          RoomUser[code][user.id] = {
+            name: user.name,
+            x: 1800,
+            y: 1800,
+            direction: "down",
+            state: "mid",
+            cId: user.cId,
+          };
 
-		// socket.on(TYPE.LEAVE_ROOM, async (user: IUser, code: any) => {
-		// 	socket.leave(code);
-		// 	if (user.id) {
-		// 		try {
-		// 			delete RoomUser[code][user.id];
-		// 		} catch (e) {
-		// 			console.log(e);
-		// 		}
-		// 		socketIO.to(code).emit("makeRoomClient", code, RoomUser[code]);
-		// 		socketIO.to(code).emit("changeMove", RoomUser[code], null);
-		// 	}
-		// });
+          done({
+            name: user.name,
+            x: 1800,
+            y: 1800,
+            direction: "down",
+            state: "mid",
+            cId: user.cId,
+          });
+          socketIO.to(code).emit("makeRoomClient", code, RoomUser[code]);
+        }
+      }
+    );
 
-		// socket.on(TYPE.SEND_MESSAGE, (props: any) => {
-		// 	const { user, text, room, state } = props;
-		// 	if (state === "Everyone") {
-		// 		socketIO.to(room).emit("receiveMessage", { user, text, state });
-		// 	} else if (state === "Nearby") {
-		// 		findAdjacentUserList(user, room).map((element: string) =>
-		// 			socketIO.to(element).emit("receiveMessage", { user, text, state })
-		// 		);
-		// 	} else {
-		// 		const toSocket = findSocket(state);
-		// 		const senderSocket = findSocket(user.id);
-		// 		socketIO.to(toSocket).emit("receiveMessage", { user, text, state });
-		// 		socketIO.to(senderSocket).emit("receiveMessage", { user, text, state });
-		// 	}
-		// });
+    socket.on(TYPE.LEAVE_ROOM, async (user: IUser, code: any) => {
+      socket.leave(code);
+      if (user.id) {
+        try {
+          delete RoomUser[code][user.id];
+        } catch (e) {
+          console.log(e);
+        }
+        socketIO.to(code).emit("makeRoomClient", code, RoomUser[code]);
+        socketIO.to(code).emit("changeMove", RoomUser[code], null);
+      }
+    });
 
-		// socket.on(TYPE.CHARACTER_MOVE, (props: any) => {
-		// 	const user: IUser = props.user;
-		// 	const code: string = props.room;
-		// 	const characterPosition: any = props.characterPosition;
-		// 	const tempBackground: any = props.tempBackground;
-		// 	if (user.id) {
-		// 		RoomUser[code][user.id] = characterPosition
-		// 			? {
-		// 					...characterPosition,
-		// 					name: user.name,
-		// 			  }
-		// 			: {
-		// 					name: user.name,
-		// 					x: findDefaultCharacterPosition(RoomList[code]).x,
-		// 					y: findDefaultCharacterPosition(RoomList[code]).y,
-		// 					direction: "down",
-		// 					state: "mid",
-		// 					cId: user.cId,
-		// 			  };
-		// 		socketIO
-		// 			.to(code)
-		// 			.emit("changeMove", RoomUser[code], isContain(tempBackground, RoomList[code]) ? tempBackground : undefined);
-		// 	}
-		// });
-		socket.on("join-room", (name: string) => {
-			const newUser: IUser = {
-				name: name,
-				id: Math.random().toString(32).substring(2, 11),
-				cId: Math.random().toString(32).substring(2, 11),
-			};
-			socket.join(GATHER_ROOM_CODE);
-			socket.emit("set-user", newUser);
+    socket.on(TYPE.CHARACTER_MOVE, (props: any) => {
+      const user: any = props.user;
+      const code: string = props.room;
+      const tempBackground: any = props.tempBackground;
+      if (socket.id) {
+        RoomUser[code][socket.id] = user
+          ? {
+              ...user,
+            }
+          : {
+              name: user.name,
+              x: findDefaultCharacterPosition(RoomList[code]).x,
+              y: findDefaultCharacterPosition(RoomList[code]).y,
+              direction: "down",
+              state: "mid",
+              cId: user.cId,
+            };
+        socketIO
+          .to(code)
+          .emit(
+            "changeMove",
+            RoomUser[code],
+            isContain(tempBackground, RoomList[code])
+              ? tempBackground
+              : undefined
+          );
+      }
+    });
 
-			userSocketList.push({ id: newUser.id, socket: socket.id });
-			RoomUser[GATHER_ROOM_CODE][newUser.id] = {
-				name: newUser.name,
-				x: 1800,
-				y: 1800,
-				direction: "down",
-				state: "mid",
-				cId: newUser.cId,
-			};
-
-			socket.emit("users-changed", RoomUser[GATHER_ROOM_CODE]);
-			socket.to(GATHER_ROOM_CODE).emit("users-changed", RoomUser[GATHER_ROOM_CODE]);
-		});
-
-		socket.on("leave-room", async (user: IUser) => {
-			socket.leave(GATHER_ROOM_CODE);
-			if (user.id) {
-				try {
-					delete RoomUser[GATHER_ROOM_CODE][user.id];
-				} catch (e) {
-					console.log(e);
-				}
-				socketIO.to(GATHER_ROOM_CODE).emit("users-changed");
-				// socketIO.to(GATHER_ROOM_CODE).emit("makeRoomClient", GATHER_ROOM_CODE, RoomUser[GATHER_ROOM_CODE]);
-				// socketIO.to(GATHER_ROOM_CODE).emit("changeMove", RoomUser[GATHER_ROOM_CODE], null);
-			}
-		});
-		socket.on("send-chat", (chat: IChat) => {
-			// socket.broadcast(GATHER_ROOM_CODE).emit("receive-chat", chat);
-			socket.broadcast.emit("receive-chat", chat);
-		});
-		socket.on("disconnect", () => {});
-	});
+    socket.on(TYPE.SEND_MESSAGE, (chat: IChat) => {
+      const { status } = chat;
+      if (status === "Everyone") {
+        socket.broadcast.emit(TYPE.RECEIVE_MESSAGE, chat);
+      } else if (status === "Nearby") {
+        findAdjacentUserList(
+          RoomUser[GATHER_ROOM_CODE][socket.id],
+          GATHER_ROOM_CODE
+        ).map((element: string) =>
+          socketIO.to(element).emit(TYPE.RECEIVE_MESSAGE, chat)
+        );
+      } else {
+        socket.to(status).emit(TYPE.RECEIVE_MESSAGE, chat);
+      }
+    });
+    socket.on("disconnect", () => {
+      console.log("disconnect", socket.id);
+    });
+  });
 };
 
 export { getSocket };
